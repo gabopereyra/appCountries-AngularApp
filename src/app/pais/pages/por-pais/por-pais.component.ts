09f68c9d5fd9093f28fcf09dde9011d96422e7c6
gabopereyra <gabrielp1689@gmail.com>
2023-01-09 20:27:05 -0300

    ADD: pais.service.ts -> service to consume API and returns countries by name app.module.ts -> HttpClientModule added pais.module.ts -> FormsModule added por-pais-component.ts -> method to consume service added por-pais-component.html -> add structure and properties to send query for service

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styleUrls: ['./por-pais.component.css']
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent{
 
-  constructor() { }
+  public termino : string = '';
+  public ocurrioError : boolean = false;
 
-  ngOnInit(): void {
+  constructor(
+    private paisService : PaisService,
+  ) { }
+
+  buscar(){
+    this.ocurrioError = false;
+    this.paisService.buscarPais(this.termino)
+      .subscribe(
+        { 
+          next: (v) => this.ocurrioError = false,
+          error: (e) => {this.ocurrioError = true},
+        }
+      );
   }
-
 }
+
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private _url : string = "https://restcountries.com/v3.1";
+
+  constructor(private httpClient : HttpClient) {}
+
+  buscarPais(termino: string) : Observable<any>{
+    const url = `${this._url}/name/${termino}`;
+
+    return this.httpClient.get(url);
+  }
+}
