c7715fdeeae06df43afa50b0da6f496dc5dbe53e
gabopereyra <gabrielp1689@gmail.com>
2023-01-10 22:28:29 -0300

    Add: debounce
    Change: placeholder attr on html

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,19 +1,37 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Country } from '../../interfaces/country.interface';
-import { PaisService } from '../../services/pais.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
   styleUrls: ['./pais-input.component.css']
 })
-export class PaisInputComponent{
+export class PaisInputComponent implements OnInit {
   @Output() onEnterTermino : EventEmitter<string> = new EventEmitter();
 
+  @Output() onDebounce : EventEmitter<string> = new EventEmitter();
+
+  dobouncer : Subject<string> = new Subject();
+
+  @Input() _placeholder : string = "";
+
   public termino: string = '';
+
+  ngOnInit(){
+    this.dobouncer
+    .pipe(debounceTime(300))
+    .subscribe(value => {
+      this.onDebounce.emit(value);
+    });
+  }
  
   buscar() {
     this.onEnterTermino.emit(this.termino);
   }
 
+  teclaPresionada(){
+    this.dobouncer.next(this.termino);
+  }
 }
