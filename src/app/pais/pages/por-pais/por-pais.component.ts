adaebc370112ae5bbd65ef9953d18c744f2c7b9e
gabopereyra <gabrielp1689@gmail.com>
2023-01-12 08:59:41 -0300

    Add function to display suggestions in accordance with text in input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -11,11 +11,14 @@ export class PorPaisComponent {
   public termino: string = '';
   public ocurrioError: boolean = false;
   public countries: Country[] = [];
-  public _placeholder : string = "Buscar por pais"
+  public paisesSugeridos: Country[] = [];
+  public _placeholder : string = "Buscar por pais";
+  public mostrarSugerencias : boolean = false;
 
   constructor(private paisService: PaisService) {}
 
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
     this.ocurrioError = false;
     this.paisService
       .buscarPais(termino)
@@ -30,7 +33,25 @@ export class PorPaisComponent {
       });
   }
   
-  sugerencias(event : string){
+  sugerencias(termino : string){
+    this.ocurrioError = false;
+    this.mostrarSugerencias = true;
+    this.termino = termino;
 
+    this.paisService
+      .buscarPais(termino)
+      .subscribe({
+        next: (response) => {
+          this.paisesSugeridos = response;
+        },
+        error: (e) =>{
+          this.paisesSugeridos = [];
+        },
+      });
+  }
+
+
+  buscarSugerido(termino : string){
+    this.buscar(termino);
   }
 }
